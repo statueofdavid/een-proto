f507976a5559aaceede6d26f52463219d275cfcb
sodvm <dmpersonalemail@gmail.com>
2024-08-29 23:44:29 -0400

    simple report layout

--- routes.js
+++ routes.js
@@ -4,8 +4,13 @@ const router = express.Router();
 console.log(`express router module loaded :: ${router}`);
 
 router.get('/dashboard', async (req, res) =>  {
-  console.log('dashboard served');
+  console.log('rendering dashboard');
   res.render('dashboard');
+});
+
+router.get('*', async (req, res) => {
+  console.log('page does not exist');
+  res.render('fourzerofour');
 });
 
 module.exports = router;
